3652b26c89d6e02a53388df746d856d579faf54a
Ege <egecam000@gmail.com>
2022-03-22 01:41:57 +0300

    update classname

--- components/header.js
+++ components/header.js
@@ -5,19 +5,19 @@ export default function Header() {
     <header className="site-container py-6">
       <nav className="space-x-8">
         <Link href="/">
-          <a className="link-hover p-1">About</a>
+          <a className="link-hover link">About</a>
         </Link>
 
         <Link href="/music">
-          <a className="link-hover p-1">Music</a>
+          <a className="link-hover link">Music</a>
         </Link>
 
         <Link href="/photography">
-          <a className="link-hover p-1">Photography</a>
+          <a className="link-hover link">Photography</a>
         </Link>
 
         <Link href="/blog">
-          <a className="link-hover p-1">Blog</a>
+          <a className="link-hover link">Blog</a>
         </Link>
       </nav>
     </header>
