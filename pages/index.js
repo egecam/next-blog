fc67862c18469414da462c48c7136cf305e50575
Ege <egecam000@gmail.com>
2022-03-21 16:56:27 +0300

    add homepage components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 function HomePage() {
-  return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>;
+  return (
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <div className="max-w-4xl mx-auto mt-20">
+            <img src="/img/laptop.png" alt="" />
+          </div>
+          <h1 className="text-2xl font-bold">Hey 👋🏻 I'm Ege.</h1>
+          <p>I am a software developer based in İstanbul.</p>
+
+          <p>
+            I am interested in app development, music, writing and photography.
+          </p>
+
+          <p>Send me an email to meet or talk for a coffee.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
