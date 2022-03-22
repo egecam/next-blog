cfa8088f31f7f8ec14492abca13c8ece57181834
Ege <egecam000@gmail.com>
2022-03-23 00:00:18 +0300

    fetchComment

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -2,17 +2,36 @@ import { getMdxNode, getMdxPaths } from "next-mdx/server";
 import { useHydrate } from "next-mdx/client";
 import { mdxComponents } from "../../components/mdx-component";
 import { useAuth0 } from "@auth0/auth0-react";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import Form from "../../components/form";
+
+import Comments from "../../components/comments";
 
 export default function PostPage({ post }) {
-  const { loginWithRedirect, isAuthenticated, user, logout, getAccessTokenSilently } = useAuth0();
-  const [text, textSet] = useState("")
-  const [url, urlSet] = useState(null)
+  const { getAccessTokenSilently } = useAuth0();
+  const [text, textSet] = useState("");
+  const [url, urlSet] = useState(null);
+  const [comments, commentsSet] = useState([]);
+
+  const fetchComment = async () => {
+    const query = new URLSearchParams({ url });
+    const newUrl = `/api/comment?${query.toString()}`;
+    const response = await fetch(newUrl, {
+      method: "GET",
+    });
+    const data = await response.json()
+    commentsSet(data);
+  };
 
   useEffect(() => {
-    const url = window.location.origin + window.location.pathname
-    urlSet(url)
-  }, [])
+    if (!url) return;
+    fetchComment();
+  }, [url]);
+
+  useEffect(() => {
+    const url = window.location.origin + window.location.pathname;
+    urlSet(url);
+  }, []);
 
   const content = useHydrate(post, {
     components: mdxComponents,
@@ -23,15 +42,15 @@ export default function PostPage({ post }) {
 
     const userToken = await getAccessTokenSilently();
     // text, user, url
-    const response = await fetch("/api/comment", {
+    await fetch("/api/comment", {
       method: "POST",
       body: JSON.stringify({ text, userToken, url }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
-    const data = await response.json();
-    console.log(data);
+    fetchComment();
+    textSet("");
   };
 
   return (
@@ -43,41 +62,9 @@ export default function PostPage({ post }) {
         <div className="prose">{content}</div>
       </article>
 
-      <form className="mt-10" onSubmit={onSubmit}>
-        <textarea
-          rows="3"
-          className="border border-gray-300 rounded-l w-full block px-2 py-1"
-          onChange={(e)=> textSet(e.target.value)}
-        ></textarea>
+      <Form onSubmit={onSubmit} text={text} textSet={textSet} />
 
-        <div className="mt-4">
-          {isAuthenticated ? (
-            <div className="flex items-center space-x-2">
-              <button className="bg-yellow-500 hover:bg-yellow-400 transition-all text-white px-2 py-1 rounded">
-                Send
-              </button>
-              <img src={user.picture} width={30} className="rounded-full" />
-              <span>{user.name}</span>
-              <button
-                typeof="button"
-                onClick={() =>
-                  logout({ returnTo: `${process.env.NEXT_PUBLIC_URL}/blog` })
-                }
-              >
-                x
-              </button>
-            </div>
-          ) : (
-            <button
-              className="bg-yellow-500 hover:bg-yellow-400 transition-all text-white px-2 py-1 rounded"
-              typeof="button"
-              onClick={() => loginWithRedirect()}
-            >
-              Login
-            </button>
-          )}
-        </div>
-      </form>
+      <Comments comments={comments} />
     </div>
   );
 }
