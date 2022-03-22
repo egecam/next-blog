2360da54a7d18c7c2364bc3a1a59cdd830c500c2
Ege <egecam000@gmail.com>
2022-03-23 01:32:54 +0300

    [WIP] create photography page

--- pages/photography.js
+++ pages/photography.js
@@ -1,5 +1,29 @@
 function PhotoPage() {
-  return <div className="site-container">photos</div>
+  return (
+    <>
+      <div className="site-container">
+        <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+          <img
+            src="/photography/betrayal.jpg"
+            alt=""
+            className="wide-img"
+            title="Betrayal"
+          />
+          <img
+            src="/photography/bosphorus from mihrabat.jpg"
+            alt=""
+            className="tight-img"
+            title="Bosphorus from Mihrabat"
+          />
+          <img
+            src="/photography/betrayal.jpg"
+            alt=""
+            className="tight-img"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PhotoPage;
