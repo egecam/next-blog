5c676b8d6c5fe9b5aedc5a18c759d98a15d8225c
Ege <egecam000@gmail.com>
2022-04-05 20:25:59 +0300

    Create contact.js

---
+++ components/contact.js
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import { FaTwitter, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+
+export default function Contact() {
+  return (
+    <div className="py-6">
+      <div className="mt-5">
+        <Link href="mailto:egecam000@gmail.com">
+          <a className="link inline-flex">
+            <FiMail className="mr-2" />
+            <p>Send me an email to meet online or for a coffee.</p>
+          </a>
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Link href="https://www.twitter.com/egecamx">
+          <a className="link inline-flex ">
+            <FaTwitter className="mr-2" />
+            <p>I often use Twitter. Don't hesitate to tweet or DM me.</p>
+          </a>
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Link href="https://www.instagram.com/egecaam">
+          <a className="link inline-flex">
+            <FaInstagram className="mr-2" />
+            <p>
+              I share some stuff about me and my friends or music on Instagram.
+            </p>
+          </a>
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Link href="https://www.github.com/egecam">
+          <a className="link inline-flex">
+            <FaGithub className="mr-2" />
+            <p>See my GitHub portfolio for my projects.</p>
+          </a>
+        </Link>
+      </div>
+
+      <div className="mt-8">
+        <Link href="https://www.linkedin.com/in/egecam">
+          <a className="link inline-flex">
+            <FaLinkedin className="mr-2" />
+            <p>LinkedIn for business related stuff.</p>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
