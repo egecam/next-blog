95d82041ce00115466b378d8c185c0680ae34b38
Ege <egecam000@gmail.com>
2022-04-05 20:27:36 +0300

    add wip sign

--- pages/photography.js
+++ pages/photography.js
@@ -2,7 +2,8 @@ function PhotoPage() {
   return (
     <>
       <div className="site-container">
-        <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+        Work in progress
+        {/*<div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
           <img
             src="/photography/betrayal.jpg"
             alt=""
@@ -20,7 +21,7 @@ function PhotoPage() {
             alt=""
             className="tight-img"
           />
-        </div>
+        </div>*/}
       </div>
     </>
   );
