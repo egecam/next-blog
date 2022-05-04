37522a481ef1e603918f0032f7082ef8a1028ef0
Ege <egecam000@gmail.com>
2022-05-04 20:19:45 +0300

    add target blank attribute to the links

--- components/contact.js
+++ components/contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
     <div className="py-6">
       <div className="mt-5">
         <Link href="mailto:egecam000@gmail.com">
-          <a className="link inline-flex">
+          <a className="link inline-flex" target="_blank">
             <FiMail className="mr-2" />
             <p>Send me an email to meet online or for a coffee.</p>
           </a>
@@ -16,7 +16,7 @@ export default function Contact() {
 
       <div className="mt-8">
         <Link href="https://www.twitter.com/egecamx">
-          <a className="link inline-flex ">
+          <a className="link inline-flex" target="_blank">
             <FaTwitter className="mr-2" />
             <p>I often use Twitter. Don't hesitate to tweet or DM me.</p>
           </a>
@@ -25,7 +25,7 @@ export default function Contact() {
 
       <div className="mt-8">
         <Link href="https://www.instagram.com/egecaam">
-          <a className="link inline-flex">
+          <a className="link inline-flex" target="_blank">
             <FaInstagram className="mr-2" />
             <p>
               I share some stuff about me and my friends or music on Instagram.
@@ -36,7 +36,7 @@ export default function Contact() {
 
       <div className="mt-8">
         <Link href="https://www.github.com/egecam">
-          <a className="link inline-flex">
+          <a className="link inline-flex" target="_blank">
             <FaGithub className="mr-2" />
             <p>See my GitHub portfolio for my projects.</p>
           </a>
@@ -45,7 +45,7 @@ export default function Contact() {
 
       <div className="mt-8">
         <Link href="https://www.linkedin.com/in/egecam">
-          <a className="link inline-flex">
+          <a className="link inline-flex" target="_blank">
             <FaLinkedin className="mr-2" />
             <p>LinkedIn for business related stuff.</p>
           </a>
